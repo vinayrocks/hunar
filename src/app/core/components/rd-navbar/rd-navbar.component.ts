aa6fc3d66e23838ff7c267b63549d1cd4c942a6b
vinay <vinayrocks12691@gmail.com>
2022-05-10 23:17:48 +0530

    changes in navbar

--- src/app/core/components/rd-navbar/rd-navbar.component.ts
+++ src/app/core/components/rd-navbar/rd-navbar.component.ts
@@ -36,29 +36,31 @@ export class RdNavbarComponent implements OnInit {
             }
         });
     }
-    sidebarOpen() {
-        const toggleButton = this.toggleButton;
-        const html = document.getElementsByTagName('html')[0];
-        setTimeout(function () {
-            toggleButton.classList.add('toggled');
-        }, 500);
-        html.classList.add('nav-open');
-        this.sidebarVisible = true;
-    };
-    sidebarClose() {
-        const html = document.getElementsByTagName('html')[0];
-        // console.log(html);
-        this.toggleButton.classList.remove('toggled');
-        this.sidebarVisible = false;
-        html.classList.remove('nav-open');
-    };
-    sidebarToggle() {
-      if (this.sidebarVisible === false) {
-          this.sidebarOpen();
-      } else {
-          this.sidebarClose();
-      }
-  };
+//     sidebarOpen() {
+//         const toggleButton = document.getElementById('myTopnav');
+//         const html = document.getElementsByTagName('html')[0];
+//         setTimeout(function () {
+//             toggleButton.classList.add('toggled');
+//         }, 500);
+//         html.classList.add('nav-open');
+//         this.sidebarVisible = true;
+//     };
+//     sidebarClose() {
+//         const toggleButton = document.getElementById('myTopnav');
+//         const html = document.getElementsByTagName('html')[0];
+//         // console.log(html);
+//         toggleButton.classList.remove('toggled');
+//         this.sidebarVisible = false;
+//         html.classList.remove('nav-open');
+//     };
+//     sidebarToggle() {
+//         debugger
+//       if (this.sidebarVisible === false) {
+//           this.sidebarOpen();
+//       } else {
+//           this.sidebarClose();
+//       }
+//   };
 
     openDialog() {
         const dialogConfig = new MatDialogConfig();
